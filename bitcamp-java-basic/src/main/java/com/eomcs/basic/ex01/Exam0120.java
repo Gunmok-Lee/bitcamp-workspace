032428f11cb8408308fb1a6a8d1bd00f414437c3
Gunmok-Lee <Gunmok.L@gmail.com>
2020-07-16 17:55:31 +0900

    실습 및 내용 수정2

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex01/Exam0120.java
@@ -0,0 +1,11 @@
+// Exam0120.java
+// 주제: 한 소스 파일 여러 클래스
+
+package com.eomcs.basic.ex01;
+
+class A {}
+class B {}
+class C {}
+
+//한 소스코드에 한 데이터코드(class)파일이 만들어지는게 아니라
+//각각의 class에 따라 여러개의 데이터코드 파일이 만들어진다.
---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex01/Exam0220.java
@@ -0,0 +1,12 @@
+// Exam0220.java
+// 주제: 소스 파일명과 클래스명의 관계
+
+package com.eomcs.basic.ex01;
+
+public class Exam0220x {
+    // 자바 명령 작성
+}
+
+//컴파일이 안되는 이유 
+//class를 공개한다는 public이 붙으면 파일명과 클래스가 같아야 한다.
+//이유는 유지보수를 쉽게 하기 위함이다.
