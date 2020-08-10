30233379ddab548fd78fe4734fe4d6192af38271
이건목 <antoniolee@igeonmog-ui-MacBookPro.local>
2020-08-10 19:49:11 +0900

    8/10 study

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex04/Exam0130a.java
@@ -0,0 +1,11 @@
+package com.eomcs.oop.ex04;
+
+import java.util.Calendar;
+
+public class Exam0130a {
+  public static void main(String[] args) throws Exception {
+    Calendar c1 = Calendar.getInstance();
+    Calendar c2 = Calendar.getInstance();
+    System.out.println(c1 == c2);
+  }
+}
