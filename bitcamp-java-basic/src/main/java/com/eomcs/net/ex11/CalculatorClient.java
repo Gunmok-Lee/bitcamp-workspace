1c670b050bb53c631b675388daddca12e9ba6a35
이건목 <antoniolee@igeonmog-ui-MacBookPro.local>
2020-10-05 18:10:30 +0900

    10/05 study

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/net/ex11/CalculatorClient.java
@@ -0,0 +1,32 @@
+package com.eomcs.net.ex11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class CalculatorClient {
+  public static void main(String[] args) {
+
+    try (Socket socket = new Socket("localhost", 8888);
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+      readResponse(in);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  static void readResponse(BufferedReader in) throws Exception {
+    while (true) {
+      String input = in.readLine();
+      if (input.length() == 0) {
+        break;
+      }
+      System.out.println(input);
+
+    }
+  }
+}
