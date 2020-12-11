4be392d3f70771d733c6657ae055620ed66c0d35
이건목 <antoniolee@igeonmog-ui-MacBookPro.local>
2020-12-11 15:28:32 +0900

    12/11 study

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/web/listener/AutoLoginListener.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/web/listener/AutoLoginListener.java
@@ -1,27 +1,29 @@
 package com.eomcs.pms.web.listener;
 
+import java.util.Map;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import com.eomcs.pms.domain.Member;
-import com.eomcs.pms.service.MemberService;
 
 @WebListener
 public class AutoLoginListener implements ServletRequestListener {
-
+  @SuppressWarnings("unchecked")
   @Override
   public void requestInitialized(ServletRequestEvent sre) {
-    System.out.println("세션 생성되었음! - ㅋㅋ 자동 로그인!");
-    try {
-      HttpSession session = ((HttpServletRequest)sre.getServletRequest()).getSession();
-      MemberService memberService =
-          (MemberService) session.getServletContext().getAttribute("memberService");
-      Member member = memberService.get("f2@test.com", "1111");
+    System.out.println("ㅋㅋ 자동 로그인!");
+    HttpSession session = ((HttpServletRequest)sre.getServletRequest()).getSession();
+
+    if (session.getAttribute("loginUser") == null) {
+
+      Map<String,Object> beanContainer =
+          (Map<String,Object>) sre.getServletContext().getAttribute("beanContainer");
+
+      Member member = new Member().setNo(22).setName("bit").setEmail("bit@test.com");
+
       session.setAttribute("loginUser", member);
-    } catch (Exception e) {
-      e.printStackTrace();
     }
   }
 }
--- bitcamp-java-project-server/src/main/webapp/index.jsp
+++ bitcamp-java-project-server/src/main/webapp/index.jsp
@@ -8,5 +8,6 @@
 </head>
 <body>
 <h1>Hello, JSP!</h1>
+<p>okok!</p>
 </body>
 </html>
